b43349c8db399917352efe4c033ada136d2be808
Yuvraj <fcyuvraj18@gmail.com>
2025-11-12 16:53:35 +0530

    Update App.js

--- studentList/src/App.js
+++ studentList/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Student from "./Student";
 
-const studentsData = [
+const studentsData = [ // array of objects
   { name: "Yuvi", grade: "A (top)" , age:19},
   { name: "papu", grade: "B", age:20 },
   { name: "sourhadya", grade: "E (Fail)",age:19 },
   { name: "Dhruv", grade:"B (papu 2)", age:20}
 ];
+
+// do the same on a object of arrays using fucntions like Object.Keys etc
 
 const App = () => {
   return (
